8aeff771beff50bc50996dd888521acac95b4f42
HammadAshrafNawab <53851100+HammadAshrafNawab@users.noreply.github.com>
2021-04-21 21:41:07 +0500

    f

---
+++ linktojson/Default.aspx
@@ -0,0 +1,86 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="linktojson._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+   <table>
+
+            <tr>
+                <th>ID</th>
+                <td>
+                    <asp:TextBox ID="txtid" runat="server"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click" />
+                </td>
+            </tr>
+            <tr>
+                <th>Title</th>
+                <td>
+                    <asp:TextBox ID="txttitle" runat="server"></asp:TextBox>
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <th>Unit_Price</th>
+                <td>
+                    <asp:TextBox ID="txtunit" runat="server"></asp:TextBox>
+                </td>
+                <td></td>
+            </tr>
+            <tr>
+                <th>Expire</th>
+                <td>
+                    <asp:TextBox ID="txtexpire" runat="server"></asp:TextBox>
+                </td>
+                <td></td>
+            </tr>
+                 <tr>
+                <th>Manufacture</th>
+                <td>
+                    <asp:TextBox ID="txtmanu" runat="server"></asp:TextBox>
+                </td>
+                <td></td>
+            </tr>
+        </tr>
+                 <tr>
+                <th>Stock</th>
+                <td>
+                    <asp:TextBox ID="txtstock" runat="server"></asp:TextBox>
+                </td>
+                <td></td>
+            </tr>
+
+
+           
+            <tr>
+                <td>
+                    <asp:Button ID="btnAdd" runat="server" Text="ADD" OnClick="btnAdd_Click1" />
+                </td>
+                <td>
+                    <asp:Button ID="btnUpdate" runat="server" Text="UPDATE" OnClick="btnUpdate_Click"  />
+                </td>
+                <td>
+                    <asp:Button ID="btnDelete" runat="server" Text="DELETE" OnClick="btnDelete_Click" />
+                </td>
+            </tr>
+       <tr>
+
+               
+
+                <td colspan="3">
+                    <asp:TextBox ID="main" runat="server"></asp:TextBox>
+                     <asp:Button ID="Button1" runat="server" Text="Save To File" OnClick="Button1_Click" />
+                </td>
+            </tr>
+            <tr>
+
+               
+
+                <td colspan="3">
+                    <asp:GridView ID="GridView1" runat="server" ></asp:GridView>
+                </td>
+            </tr>
+        </table>
+
+
+</asp:Content>
---
+++ linktojson/Default.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace linktojson
+{
+    public class Products
+        {
+        public string ID { get; set; }
+        public string TITLE { get; set; }
+        public string UNIT_PRICE { get; set; }
+        public string EXPIRE { get; set; }
+        public string MANUFACTURE { get; set; }
+        public string STOCK { get; set; }
+
+
+    }
+    public partial class _Default : Page
+    {
+        List<Dictionary<string, object>> rows;
+        System.Web.Script.Serialization.JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            StreamReader reader = new StreamReader(Server.MapPath("Product.json"));
+            string json = reader.ReadToEnd();
+            rows = serializer.Deserialize<List<Dictionary<string, object>>>(json);
+            reader.Close();
+            if(!IsPostBack)
+            {
+                main.Text = serializer.Serialize(rows);
+                LoadGridView();
+            }
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, object> row = new Dictionary<string, object>();
+            row.Add("P_ID", txtid.Text);
+            row.Add("P_TITLE", txttitle.Text);
+            row.Add("P_UNIT_PRICE", txtunit.Text);
+            row.Add("P_EXPIRE", txtexpire.Text);
+            row.Add("P_MANUFACTURE", txtmanu.Text);
+            row.Add("P_STOCK", txtstock.Text);
+            rows.Add(row);
+            main.Text = serializer.Serialize(rows);
+            LoadGridView();
+
+
+
+        }
+        private void LoadGridView()
+        {
+            var data = rows.Select(x => new Products
+            {
+
+                ID = x["P_ID"].ToString(),
+                TITLE = x["P_TITLE"].ToString(),
+                UNIT_PRICE = x["UNIT_PRICE"].ToString(),
+                EXPIRE = x["P_EXPIRE"].ToString(),
+                MANUFACTURE = x["P_MANUFACTURE"].ToString(),
+                STOCK = x["P_STOCK"].ToString()
+              
+            })
+            ;
+            GridView1.DataSource = data;
+            GridView1.DataBind();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, object> row = new Dictionary<string, object>();
+            row.Add("P_ID", txtid.Text);
+            row = rows.SingleOrDefault(x => row.All(x.Contains));
+
+            row.Add("P_TITLE", txttitle.Text);
+            row.Add("P_UNIT_PRICE", txtunit.Text);
+            row.Add("P_EXPIRE", txtexpire.Text);
+            row.Add("P_MANUFACTURE", txtmanu.Text);
+            row.Add("P_STOCK", txtstock.Text);
+            main.Text = serializer.Serialize(rows);
+            LoadGridView();
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, object> row = new Dictionary<string, object>();
+            row.Add("P_ID", txtid.Text);
+            row = rows.SingleOrDefault(x => row.All(x.Contains));
+            txttitle.Text = row["P_TITLE"].ToString();
+            txtunit.Text = row["P_UNIT_PRICE"].ToString();
+            txtexpire.Text = row["P_EXPIRE"].ToString();
+            txtmanu.Text = row["P_MANUFACTURE"].ToString();
+            txtstock.Text = row["P_STOCK"].ToString();
+
+        }
+
+        protected void btnAdd_Click1(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            System.IO.StreamWriter myFile = new System.IO.StreamWriter(Server.MapPath("Product.json"));
+            if(!main.Text.Equals(""))
+            {
+                myFile.WriteLine(main.Text);
+            }
+            myFile.Close();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, object> row = new Dictionary<string, object>();
+            row.Add("P_ID", txtid.Text);
+            row = rows.SingleOrDefault(x => row.All(x.Contains));
+            rows.Remove(row);
+            main.Text = serializer.Serialize(rows);
+            LoadGridView();
+
+        }
+    }
+}
